4ea6e0598076ec9eaaa8db7dcdeaac569a17dc88
howetong <howetong@whu.edu.cn>
2017-09-12 18:49:19 +0800

    1

---
+++ mysc-consumer2/src/main/java/cn/tonghao/mysc/FeignApplication.java
@@ -0,0 +1,18 @@
+package cn.tonghao.mysc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+/**
+ * Created by howetong on 9/12/2017.
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class FeignApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FeignApplication.class, args);
+    }
+}
---
+++ mysc-provider1/src/main/java/cn/tonghao/mysc/ComputeServiceApplication.java
@@ -0,0 +1,16 @@
+package cn.tonghao.mysc;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Created by howetong on 9/12/2017.
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ComputeServiceApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ComputeServiceApplication.class).web(true).run(args);
+    }
+}
