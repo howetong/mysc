59023632380b8cf2b7573235de3a4c6476536a23
howetong <howetong@blackfish.cn>
2017-09-18 18:52:01 +0800

    1

---
+++ mysc-configclient2/src/main/java/mysc/ConfigClientApplication.java
@@ -0,0 +1,16 @@
+package mysc;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Created by howetong on 2017/9/18.
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ConfigClientApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ConfigClientApplication.class).web(true).run(args);
+    }
+}
---
+++ mysc-configclient2/src/main/java/mysc/controller/TestController.java
@@ -0,0 +1,25 @@
+package mysc.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by howetong on 2017/9/18.
+ */
+@RefreshScope
+@RestController
+class TestController {
+
+    @Value("${form}")
+    private String from;
+
+    @Value("${key}")
+    private String key;
+
+    @RequestMapping("/from")
+    public String from() {
+        return this.from;
+    }
+}
