00df51f9868fdc81b0731eb08d62410e6ed68c9d
howetong <howetong@blackfish.cn>
2017-09-21 15:37:03 +0800

    1

---
+++ mysc-server2/src/main/java/cn/tonghao/mysc/Application.java
@@ -0,0 +1,16 @@
+package cn.tonghao.mysc;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by howetong on 9/12/2017.
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
+}
